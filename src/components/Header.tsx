6efe35d0f856556da616a6466c131823855168f7
Daniel <daniel.liemng@gmail.com>
2023-08-31 10:31:20 -0400

    details page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,25 @@
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
+
+import { Link } from 'react-router-dom';
+import FavoriteIcon from '@mui/icons-material/Favorite';
 
 const Header = () => {
   return (
     <AppBar position='static'>
       <Toolbar>
-        <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
-          Charity Finder
+        <Typography
+          variant='h6'
+          component='div'
+          sx={{ flexGrow: 1, color: 'white', textTransform: 'uppercase' }}
+        >
+          <Link to='/'>Charity Finder</Link>
         </Typography>
-        <Button color='inherit'>Login</Button>
+
+        <Link to='/favorites'>
+          <FavoriteIcon fontSize='medium' sx={{ color: '#ff0000' }} />
+        </Link>
       </Toolbar>
     </AppBar>
   );
